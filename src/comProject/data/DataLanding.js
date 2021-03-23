6ad57eb52caf103243a996d05e54f360d14e5822
mehdi <71771200+medi-jerr@users.noreply.github.com>
2021-03-23 22:22:09 +0100

    Delete src/comProject directory

--- src/comProject/data/DataLanding.js
+++
@@ -1,7 +0,0 @@
-export const data = [
-  "/img/one.jpg",
-  "/img/two.jpg",
-  "/img/three.jpg",
-  "/img/four.jpg",
-  "/img/five.jpg",
-];
--- src/comProject/style/themes/theme.js
+++
@@ -1,5 +0,0 @@
-export default {
-  color: {
-    second: "#ff9800",
-  },
-};
